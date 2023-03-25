0b78fb2feafbf0261fb78b2fdf8b6f4a1c702262
Walid Harkous <harkousw@wit.edu>
2023-03-25 11:28:40 -0400

    Added project files

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [
+          Center(child: Text("Open AI Image Generation")),
+        ],
+      ),
+    );
+  }
+}
